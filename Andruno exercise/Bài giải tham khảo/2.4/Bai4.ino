75f6fbae32497c03ca2e4ed99ea707418d7edc30
Pink-Quan <95110396+Pink-Quan@users.noreply.github.com>
2022-06-22 09:40:38 +0700

    Update Bai4.ino

--- Andruno exercise/Bài giải tham khảo/2.4/Bai4.ino
+++ Andruno exercise/Bài giải tham khảo/2.4/Bai4.ino
@@ -1,32 +1,16 @@
 
 void setup() {
-  pinMode(9, INPUT);
-  for (int i = 0; i < 9; i++)
-    pinMode(i, OUTPUT);
-
+  pinMode(1, INPUT);
+  pinMode(0, OUTPUT);
+  digitalWrite(0,1);
 }
+
+int status=1;
 
 void loop() {
   if(digitalRead(9))
   {
-    digitalWrite(1,1);
-    digitalWrite(2,1);
-    digitalWrite(3,1);
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,1);
-  }
-  else
-  {
-    digitalWrite(1,0);
-    digitalWrite(2,0);
-    digitalWrite(3,0);
-    digitalWrite(4,0);
-    digitalWrite(5,0);
-    digitalWrite(6,0);
-    digitalWrite(7,0);
-    digitalWrite(8,0);
+    digitalWrite(1,status*(-1));
+    delay(200);
   }
 }
